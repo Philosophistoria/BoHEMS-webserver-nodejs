44b77f6b24b89b793266a6046e7152df01e0504e
Akifumi Takahashi <aktk.j.uec@gmail.com>
2021-05-03 23:24:38 +0900

    Modified server so ending up the connection when the close event occur; Modified UI;

--- public/wssettings.js
+++ public/wssettings.js
@@ -1,45 +1,70 @@
 const wsclient = {};
 //wsclient.sock = new WebSocket("ws://127.0.0.1:5001");
 wsclient.sock = new WebSocket("ws://54.168.9.34:5001");
-wsclient.playable = false;
+wsclient.is_connected_to_dev = false;
+
+const reset_connect_button = () => {
+  wsclient.button = wsclient.button || document.getElementById("connect");
+  wsclient.is_connected_to_dev = false;
+  wsclient.button.innerText = "Connect to EMS";
+}
+
+const wrilte_nl_in_logbox = (str_nl) => {
+  wsclient.logbox = wsclient.logbox || document.getElementById("log_box");
+  wsclient.logbox.innerHTML = "* " + str_nl + "<br /> " + wsclient.logbox.innerHTML;
+}
 
 wsclient.sock.addEventListener("open", e => {
   console.log("event as connected");
+  reset_connect_button();
 });
 
 wsclient.sock.addEventListener("message", e => {
   console.log("event as getting message from the server");
-  const logbox = document.getElementById("log_box");
-  const button = document.getElementById("connect");
+  wsclient.button = wsclient.button || document.getElementById("connect");
   if (e.data.match('@')){
-    str = e.data.split('@');
+    const str = e.data.split('@');
     if (str[0] === "accepted"){
-      wsclient.playable = true;
-      logbox.innerHTML = "Your turn! > @" + str[1] +  "<br />" + logbox.innerHTML;
-      button.innerText = "Disconnect";
+      wsclient.is_connected_to_dev = true;
+      wrilte_nl_in_logbox("Your turn! > @" + str[1]);
+      wsclient.button.innerText = "Dismiss now";
     }
     else if (str[0] === "rejected"){
-      logbox.innerHTML = "Sorry, now @" + str[1] + " is playing.<br />" + logbox.innerHTML;
+      wrilte_nl_in_logbox("Sorry, now @" + str[1]);
     }
     else if (str[0] === "connected"){
-      logbox.innerHTML = "@server> @" + str[1] + " gets started. <br />" + logbox.innerHTML;
+      wrilte_nl_in_logbox("@server> @" + str[1]);
     }
     else if (str[0] === "disconnected"){
-      if(wsclient.playable){
-        wsclient.playable = false;
-        logbox.innerHTML = "You has been disconnected <br />" + logbox.innerHTML;
-        button.innerText = "Connect";
+      if(wsclient.is_connected_to_dev) {
+        wrilte_nl_in_logbox("You has been disconnected");
       }
-      logbox.innerHTML = "@server> @" + str[1] + " finished. <br />" + logbox.innerHTML;
+      reset_connect_button();
+      wrilte_nl_in_logbox("@server> @" + str[1] + " finished.");
+    }
+    else if (str[0].match(':')){
+      const str_0 = str[0].split(':');
+      if(str_0[0] === "time"){
+        wsclient.button.innerText = str[1] + "'s turn ends in " + str_0[1];
+        if (wsclient.is_connected_to_dev){
+          wsclient.button.innerHTML += "<br /> Dismiss now"
+        }
+      }
+      else if(str_0[0] === "start"){
+        gwd.actions.events.setInlineStyle('electrode_' + str_0[1], 'background-color: #FFFF55;');
+      }
+      else if(str_0[0] === "stop"){
+        gwd.actions.events.setInlineStyle('electrode_' + str_0[1], 'background-color: #FFFFFF;');
+      }
+      console.log(str_0);
     }
     else {
-      logbox.innerHTML = "@server> " + e.data + "<br />" + logbox.innerHTML;
+      wrilte_nl_in_logbox("@server> " + e.data);
     }
   }
   else {
-    logbox.innerHTML = "@server> " + e.data + "<br />" + logbox.innerHTML;
+    wrilte_nl_in_logbox("@server> " + e.data);
   }
-  logbox.innerHTML = "* " + logbox.innerHTML;
 });
 
 wsclient.sock.addEventListener("close", e => {
@@ -50,3 +75,18 @@ wsclient.sock.addEventListener("error", e => {
   console.log("event as some error occurs");
 });
 
+wsclient.connect_to_dev = (name) => {
+  wsclient.sock.send(name + "> Can I try?");
+};
+
+wsclient.disconnect_from_dev = () => {
+  wsclient.sock.send("disconnect");
+};
+
+wsclient.start_dev = (index) => {
+  wsclient.sock.send("start:" + index);
+}
+
+wsclient.stop_dev = (index) => {
+  wsclient.sock.send("stop:" + index);
+}
--- wsserver.js
+++ wsserver.js
@@ -2,11 +2,11 @@ const server = require("ws").Server;
 const s = new server({ port: 5001 });
 let tryable = true;
 let current_player = '';
-const timeoutlimit = 5; //[s]
+const timeoutlimit = 180; //[s]
 let timeclock = 0;//[s]
 let intervalID = {};
 
-function connect_with(ws, player_name) {
+function connect_to(ws, player_name) {
 	timeclock = 0;
 	tryable = false;
 	ws.playable = true;
@@ -45,6 +45,11 @@ function tickUntilTimeout(ws) {
 
 s.on("connection", ws => {
 	ws.playable = false;
+	ws.on("close", () => {
+		if (ws.playable) {
+			disconnect_off(ws);
+		}
+	});
 	ws.on("message", message => {
 		console.log("Received: " + message);
 
@@ -53,7 +58,7 @@ s.on("connection", ws => {
 		}
 		else if (message.match("Can I try?")) {
 			if (tryable) {
-				connect_with(ws, message.split('>')[0])
+				connect_to(ws, message.split('>')[0])
 			}
 			else{
 				ws.send("rejected@" + current_player);
